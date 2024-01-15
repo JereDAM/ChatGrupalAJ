8712553c436b09a8b46cbd49c93f81cb6ce555f6
Albaii22 <usr3584@salesianos-lacuesta.net>
2024-01-15 01:34:43 -0800

    feat : Creacion del Client

--- src/net/salesianos/client/ClientApp.java
+++ src/net/salesianos/client/ClientApp.java
@@ -1,5 +1,40 @@
 package net.salesianos.client;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+import net.salesianos.client.threads.ServerListener;
+import net.salesianos.shared.constant.Ports;
+
 public class ClientApp {
-    
+    public static void main(String[] args) throws Exception {
+        
+        String userOption = "";
+        final Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca su nombre de usuario :");
+        String user = sc.nextLine();
+
+        Socket socket = new Socket(Ports.JEREMY_IP , Ports.SERVER_PORT);
+        ObjectOutputStream objOutStream = new ObjectOutputStream(socket.getOutputStream());
+        objOutStream.writeUTF(user);
+
+        ObjectInputStream objInStream = new ObjectInputStream(socket.getInputStream());
+        ServerListener serverListener = new ServerListener(objInStream);
+        serverListener.start();
+
+        while (userOption != "bye") {
+            userOption = sc.nextLine();
+
+            objOutStream.writeUTF(userOption);
+        }
+
+        sc.close();
+
+        objInStream.close();
+        objOutStream.close();
+        socket.close();
+    }
 }
--- src/net/salesianos/shared/constant/Ports.java
+++ src/net/salesianos/shared/constant/Ports.java
@@ -1,5 +1,6 @@
 package net.salesianos.shared.constant;
 
 public class Ports {
+    public final static String JEREMY_IP = "172.16.101.209";
     public final static int SERVER_PORT = 55000;
 }
